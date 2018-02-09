9aec1765cc3e29234adf33e6dadc0a45a4d4785d
Khoa Pham <onmyway133@gmail.com>
2018-02-09 12:16:22 +0100

    Public properties in section

--- Sources/Section.swift
+++ Sources/Section.swift
@@ -2,9 +2,9 @@ import UIKit
 
 /// Section
 public struct Section<T> {
-  let header: Header<T>?
-  let items: [Item<T>]
-  let footer: Footer<T>?
+  public let header: Header<T>?
+  public let items: [Item<T>]
+  public let footer: Footer<T>?
 
   public init(header: Header<T>? = nil, items: [Item<T>], footer: Footer<T>? = nil) {
     self.header = header
